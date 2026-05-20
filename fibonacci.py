486cc1b10402b6546f5cd474487c7b29ea7b6f01
Eganthevegan <eganlee135@gmail.com>
2026-05-20 10:33:43 -0500

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+terms = int(input("How many terms? "))
+
+for i in range(terms):
+    print(fibonacci(i), end=" ")

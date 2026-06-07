4fb1e832d1680aecec3e9516aacf2c90d0526286
Eganthevegan <eganlee135@gmail.com>
2026-06-07 11:26:00 -0500

    Add files via upload

---
+++ data structures2.py
@@ -0,0 +1,14 @@
+fruits = ["apple","mango","banana","grapes","pear","blueberry"]
+
+(fruits.append("orange"))
+print(fruits)
+(fruits.remove("mango"))
+print(fruits)
+(fruits.pop(1))
+print(fruits)
+(fruits.sort())
+print(fruits)
+(fruits.reverse())
+print(fruits)
+(fruits.clear())
+print(fruits)
